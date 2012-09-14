cfe16f03dd4bbadf81ee6f201ff612466cb628dc
Mike Browne <phelandhu@gmail.com>
2012-09-13 17:45:10 -0700

    updating minor fixes

--- ajaxTrial/index.php
+++ ajaxTrial/index.php
@@ -10,32 +10,38 @@
 ?>
 <html> 
 <head> 
-<title>Welcome!</title> 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-<script type='text/javascript'> 
-$(document).ready(function(){ 
-$("#search_results").slideUp(); 
-    $("#search_button").click(function(e){ 
-        e.preventDefault(); 
-        ajax_search(); 
-    }); 
-    $("#search_term").keyup(function(e){ 
-        e.preventDefault(); 
-        ajax_search(); 
-    }); 
+	<title>Welcome!</title>
+	<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
 
-}); 
-
-function ajax_search(){ 
-	  $("#search_results").show(); 
-	  var search_val=$("#search_term").val(); 
-	  $.post("./find.php", {search_term : search_val}, function(data){
-	   if (data.length>0){ 
-	     $("#search_results").html(data); 
-	   } 
-	  }) 
-	} 
-	</script> 
+	<script type='text/javascript'> 
+		$(document).ready(function(){ 
+			$("#search_results").slideUp(); 
+		    $("#search_button").click(function(e){ 
+		        e.preventDefault(); 
+		        ajax_search(); 
+		    }); 
+		    $("#search_term").keyup(function(e){ 
+		        e.preventDefault(); 
+				ajax_search(); 
+		    });
+	        $("#slider").slider();
+		});
+	
+		function ajax_search(){ 
+			$("#search_results").show(); 
+			var search_val=$("#search_term").val(); 
+			$.post("./find.php", {search_term : search_val}, function(data){
+				if (data.length>0){ 
+					$("#search_results").html(data); 
+				}
+			})
+		} 
+	</script>
+	<style type="text/css">
+		#slider { margin: 10px; }
+	</style>
 </head> 
 
 <body> 
